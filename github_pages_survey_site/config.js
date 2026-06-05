a79f233cd2e1f692cd344c28cfbf3b34f54fb96f
heeeeewon <73174351+heeeeewon@users.noreply.github.com>
2026-06-06 00:58:34 +0900

    Add files via upload

---
+++ github_pages_survey_site/config.js
@@ -0,0 +1,5 @@
+window.SURVEY_CONFIG = {
+  // Paste the deployed Google Apps Script Web App URL here.
+  // Example: "https://script.google.com/macros/s/AKfycb.../exec"
+  googleScriptUrl: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+};
